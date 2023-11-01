433408f9abe409383e63b9dee1ee24355309add3
m-bartos <m.bartos96@seznam.cz>
2023-11-01 14:04:06 +0100

    Tests: update main.c

--- test/main.c
+++ test/main.c
@@ -6,22 +6,28 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 16:01:51 by mbartos           #+#    #+#             */
-/*   Updated: 2023/10/31 16:18:59 by mbartos          ###   ########.fr       */
+/*   Updated: 2023/11/01 13:55:36 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
-
-//cc -Wall -Wextra -Werror ./test/main.c ft_strdup.c ft_puthexa.c ft_printf.c ft_putchar.c ft_putnbr.c ft_putstr.c ft_putunsnbr.c ft_strlen.c && ./a.out
-
 #include <stdio.h>
-int	main (void)
-{
-	char	str[] = "ahoj";
-	char	*nic;
 
-	nic = str;
-	ft_printf("Ahoj %p %x %X %u %d %s %% %s %c \naaaa.", nic, -300, -300, -15, 3, "NE", "%", 99);
+//cc -Wall -Wextra -Werror ./test/main.c *.c && ./a.out
+//("Ahoj %p %x %X %u %d %s %% %s %c \na.", nic, -30, -30, -15, 3, "NE", "%", 99)
+
+int	main(void)
+{
+	int		sum;
+	int		test;
+	char	*test_str;
+
+	test_str = NULL;
+	test = -2147483648;
+	sum = ft_printf("%x", test);
+	ft_printf("\n");
+	ft_printf("%x", test);
 	printf("\n");
-	printf("Ahoj %p %x %X %u %d %s %% %s %c \naaaa.", nic, -300, -300, -15, 3, "NE", "%", 99);
+	printf("%d", sum);
+	return (0);
 }
