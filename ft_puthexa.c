e4c2f9cd9e39e3acab8f7da72f1c9ce280b1ef3f
m-bartos <m.bartos96@seznam.cz>
2023-11-01 14:00:41 +0100

    Fix: ft_puthexa.c

--- ft_puthexa.c
+++ ft_puthexa.c
@@ -6,51 +6,25 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 14:33:33 by mbartos           #+#    #+#             */
-/*   Updated: 2023/10/31 15:55:39 by mbartos          ###   ########.fr       */
+/*   Updated: 2023/11/01 13:34:51 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	str_itoa_len(long long number)
+void	ft_puthexa(unsigned int number, char hex_base, int *ptr_sum)
 {
-	int	length;
-
-	length = 0;
-	if (number < 0)
-		length++;
-	else if (number == 0)
-		return (1);
-	while (number)
-	{
-		number = number / 16;
-		length++;
-	}
-	return (length);
-}
-
-void	ft_puthexa(long long int number, char hex_base)
-{
-	char		*str;
-	int			length;
-	char		*base;
+	char			base[17];
 
 	if (hex_base == 'h')
-		base = ft_strdup("0123456789abcdef");
+		ft_strlcpy(base, "0123456789abcdef", 17);
 	else if (hex_base == 'H')
-		base = ft_strdup("0123456789ABCDEF");
-	length = str_itoa_len(number);
-	str = (char *) malloc(sizeof(char) * (length + 1));
-	if (str == NULL)
-		return ;
-	if (number == 0)
-		str[0] = '0';
-	str[length] = '\0';
-	while (number)
+		ft_strlcpy(base, "0123456789ABCDEF", 17);
+	if (number > 15)
 	{
-		str[--length] = base[number % 16];
-		number = number / 16;
+		ft_puthexa(number / 16, hex_base, ptr_sum);
+		ft_puthexa(number % 16, hex_base, ptr_sum);
 	}
-	ft_putstr(str);
-	free(str);
+	else
+		ft_putchar_c(base[number], ptr_sum);
 }
