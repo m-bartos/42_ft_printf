2c8fb87cf39304a88601b8fccfab8948a91b0f92
m-bartos <m.bartos96@seznam.cz>
2023-11-01 14:00:11 +0100

    Update: ft_ptrintf.h

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 11:09:26 by mbartos           #+#    #+#             */
-/*   Updated: 2023/10/31 16:05:20 by mbartos          ###   ########.fr       */
+/*   Updated: 2023/11/01 13:49:45 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,17 @@
 # include <stddef.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <stdint.h>
 
+void	ft_type_choose(char type, va_list args, int *ptr_sum);
 int		ft_printf(const char *str, ...);
-void	ft_putchar(char c);
-void	ft_putnbr(int n);
-void	ft_putunsnbr(unsigned int n);
-void	ft_puthexa(long long int n, char hex_base);
-void	ft_putstr(char *s);
-void	ft_which_type(char type, va_list args);
-char	*ft_strdup(const char *str);
+void	ft_putchar_c(char c, int *ptr_sum);
+void	ft_putnbr(int n, int *ptr_sum);
+void	ft_putunsnbr(unsigned int n, int *ptr_sum);
+void	ft_puthexa(unsigned int n, char hex_base, int *ptr_sum);
+void	ft_putpointer(void *ptr, int *ptr_sum);
+void	ft_put_ptr_hexa_adress(unsigned long number, int *ptr_sum);
+void	ft_putstr_c(char *s, int *ptr_sum);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlen(const char *s);
-
 #endif
